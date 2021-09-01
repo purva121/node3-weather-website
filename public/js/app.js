79c5fe60aeb80acf30c0ae39b2aa258e897f5872
Purva <Purva.lanetteam@gmail.com>
2021-09-01 13:48:51 +0530

    Remove unneccesary console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,17 +1,7 @@
-console.log("Client side javascript file is loaded!");
-
-// fetch("https://puzzle.mead.io/puzzle").then((response) => {
-//   response.json().then((data) => {
-//     console.log(data);
-//   });
-// });
-
 const weatherForm = document.querySelector("form");
 const search = document.querySelector("input");
 const messageOne = document.querySelector("#message-1");
 const messageTwo = document.querySelector("#message-2");
-
-//messageOne.textContent = "From javascript";
 
 weatherForm.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -29,8 +19,8 @@ weatherForm.addEventListener("submit", (e) => {
         } else {
           messageOne.textContent = data.location;
           messageTwo.textContent = data.forecast;
-        //   console.log(data.location);
-        //   console.log(data.forecast);
+          //   console.log(data.location);
+          //   console.log(data.forecast);
         }
       });
     }
